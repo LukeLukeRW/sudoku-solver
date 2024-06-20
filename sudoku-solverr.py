4101840aae598a5357a4de45d1d1b9b1d63aca59
LukeLukeRW <151480620+LukeLukeRW@users.noreply.github.com>
2024-06-20 20:43:22 +1000

    Update sudoku-solverr.py

--- sudoku-solverr.py
+++ sudoku-solverr.py
@@ -5,5 +5,5 @@ root = tkinter.Tk()
 for i in range(9):
     for j in range(9):
         entry = tkinter.Entry(root, width=5, justify='center')
-        entry.grid(row=j,column=i)
+        entry.grid(row=j,column=i,padx=2,pady=2)
 root.mainloop()
