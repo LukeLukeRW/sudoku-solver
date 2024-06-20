f0458646110f3b3b5fe59d7106705a275f2795a7
LukeLukeRW <151480620+LukeLukeRW@users.noreply.github.com>
2024-06-20 21:43:23 +1000

    Update sudoku-solverr.py
    fixed a bit, had to include def submit, a nesred function to make sure only when the button was clicked it would run..

--- sudoku-solverr.py
+++ sudoku-solverr.py
@@ -14,14 +14,21 @@ def main(root):
     return entries
     
 def the_start(entries):
-    sudoku_values = []
-    for row in entries:
-        row_values = []
-        for entry in row:
-            value = entry.get()
-            row_values.append(value)
-        sudoku_values.append(row_values)
+    def submit():
+        sudoku_values = []
+        for row in entries:
+            row_values = []
+            for entry in row:
+                value = entry.get()
+                row_values.append(value)
+            sudoku_values.append(row_values)
+            
+        for i in sudoku_values:
+            for j in range(len(i)):
+                if i[j] == '':
+                    i[j] = int(0)
 
+    return submit
 
 
 if __name__ == '__main__':
@@ -32,3 +39,4 @@ if __name__ == '__main__':
 
 
     root.mainloop()
+
