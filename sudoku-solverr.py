cd2fce126644c6c5a861f80f020f64a8fb35820f
LukeLukeRW <151480620+LukeLukeRW@users.noreply.github.com>
2024-06-20 20:54:52 +1000

    Update sudoku-solverr.py

--- sudoku-solverr.py
+++ sudoku-solverr.py
@@ -1,9 +1,16 @@
-import tkinter 
+def main(root):
+    entries = []
+    for i in range(9):
+        row_entries = []
+        for j in range(9):
+            entry = tkinter.Entry(root, width=5, justify='center')
+            entry.grid(row=j,column=i,padx=2,pady=2)
+            row_entries.append(entry)
+        entries.append(row_entries)
 
-root = tkinter.Tk()
 
-for i in range(9):
-    for j in range(9):
-        entry = tkinter.Entry(root, width=5, justify='center')
-        entry.grid(row=j,column=i,padx=2,pady=2)
-root.mainloop()
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    main(root)
+    
+    root.mainloop()
