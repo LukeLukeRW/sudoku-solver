ef6fd5228c0b41017f579ba75bb7302f5fc44189
LukeLukeRW <151480620+LukeLukeRW@users.noreply.github.com>
2024-06-20 20:41:46 +1000

    Create sudoku-solver.py

---
+++ sudoku-solverr.py
@@ -0,0 +1,9 @@
+import tkinter 
+
+root = tkinter.Tk()
+
+for i in range(9):
+    for j in range(9):
+        entry = tkinter.Entry(root, width=5, justify='center')
+        entry.grid(row=j,column=i)
+root.mainloop()
